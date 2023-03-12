1d4c9bb2aa365269ccb1ddb84be2484dafc5cede
vika-sk <viktoriya-skryabina@mail.ru>
2023-03-12 13:05:05 +0300

    zadacha27

--- Program.cs
+++ Program.cs
@@ -1,29 +1,42 @@
-﻿//Задача 25:
-//Напишите метод, который принимает на вход 
-//два числа (A и B) и возводит число A в 
-//натуральную степень B.
-//3, 5 -> 243 (3⁵);   
-//2, 4 -> 16   
+﻿// Задача 27 Напишите программу, которая 
+//принимает на вход число и выдаёт сумму 
+//цифр в числе.
+//452 -> 11;  
+//82 -> 10;  
+//9012 -> 12 
 
-int numberA = ReadInt("Введите число: ");
-int numberB = ReadInt("Введите степень: ");
-ToDegree(numberA, numberB);
+int number = ReadInt("Введите число: ");
 
+int len = NumberLen(number);
+SumNumbers(number, len);
 
-// Функция возведения в степень
-void ToDegree(int a, int b)
+// Функция подсчета цифр в числе
+int NumberLen(int a)
 {
-    int result = 1;
-    for (int i = 1; i <= b; i++)
+    int index = 0;
+    while (a > 0)
     {
-        result = result * a;
+        a /= 10;
+        index++;
     }
-    Console.WriteLine(a + " в степени " + b + " = " + result);
+    return index;
+}
+
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"сумма цифр {sum}");
 }
 
 // Функция ввода
 int ReadInt(string message)
 {
-    Console.WriteLine(message);
+    Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
