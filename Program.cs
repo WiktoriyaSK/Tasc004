401ffda5217f68d8ce591905c19720c6b14d8eeb
vika-sk <viktoriya-skryabina@mail.ru>
2023-03-12 10:43:32 +0300

    urok004

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Ex007Main();
+
+
+int GetSideCube()
+{
+    return Random.Shared.Next(0, 6);
+}
+
+int GetSideCube2()
+{
+    int value = Random.Shared.Next(0, 6);
+    if (value <= 1) return Random.Shared.Next(0, 6);
+    else return Random.Shared.Next(0, 5);
+}
+
+int[] tr = new int[6];
+double n = 100;
+for (int i = 0; i < n; i++) tr[GetSideCube2()]++;
+
+Console.WriteLine(String.Join(' ', tr.Select(i=> (i / n)* 100)));
